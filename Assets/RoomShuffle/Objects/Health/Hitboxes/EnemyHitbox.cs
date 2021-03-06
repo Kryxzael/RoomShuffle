a1cb4f245ca3ce5138b88c0e270727bf15e4ad22
Kryxzael <kasper.ramskjell98@gmail.com>
2021-03-06 18:30:09 +0100

    Add hitboxes and hurtboxes. Currently don't do/receive any damage

---
+++ Assets/RoomShuffle/Objects/Health/Hitboxes/EnemyHitbox.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+/// <summary>
+/// Lets an enemy be hurt by hurtboxes
+/// </summary>
+[RequireComponent(typeof(HealthController))]
+public class EnemyHitbox : Hitbox
+{
+    private HealthController _health;
+
+    /// <summary>
+    /// <inheritdoc />
+    /// </summary>
+    public override HurtBoxTypes SusceptibleTo { get; } = HurtBoxTypes.HurtfulToEnemies;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _health = GetComponent<HealthController>();
+    }
+
+    /// <summary>
+    /// <inheritdoc />
+    /// </summary>
+    /// <param name="hurtbox"></param>
+    protected override void OnReceiveDamage(HurtBox hurtbox)
+    {
+        //TODO: Do something with _health
+    }
+}
---
+++ Assets/RoomShuffle/Objects/Health/Hitboxes/Hitbox.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+/// <summary>
+/// Lets an object detect collisions with hurtboxes
+/// </summary>
+public abstract class Hitbox : MonoBehaviour
+{
+    private SpriteRenderer _renderer; //Optional
+
+    [Tooltip("The amount of seconds the object will be invincible")]
+    public float InvincibilityFramesInSeconds = 5f;
+
+    /// <summary>
+    /// Gets the type of hurtboxes this hitbox is susceptible to
+    /// </summary>
+    public abstract HurtBoxTypes SusceptibleTo { get; }
+
+    /// <summary>
+    /// Gets whether the hitbox is currently under influence of I-frames
+    /// </summary>
+    public bool HasInvincibilityFrames { get; private set; }
+
+    protected virtual void Awake()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    /// <summary>
+    /// Coroutine: Applies invincibility frames
+    /// </summary>
+    private IEnumerator CoIFrames()
+    {
+        //Set invincible
+        HasInvincibilityFrames = true;
+
+        //Store elapsed time of I-frames, and whether the object is currently blinking
+        bool isBlinking = false;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < InvincibilityFramesInSeconds)
+        {
+            //Swap blink state
+            isBlinking = !isBlinking;
+
+            //Set opacity of sprite in accordance with current blinking state
+            if (_renderer != null)
+                _renderer.color = new Color(_renderer.color.r, _renderer.color.g, _renderer.color.b, isBlinking ? 0f : 1f);
+
+            //Wait for frame to end and increase counter
+            yield return new WaitForEndOfFrame();
+            elapsedTime += Time.deltaTime;
+        }
+
+        //Make sure the sprite is visible again when the blinking ends
+        if (_renderer != null)
+            _renderer.color = new Color(_renderer.color.r, _renderer.color.g, _renderer.color.b, isBlinking ? 0f : 1f);
+
+        //Set mortal
+        HasInvincibilityFrames = false;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (HasInvincibilityFrames)
+            return;
+
+        if (other.GetComponent<HurtBox>() is HurtBox hurt && hurt.Type.HasFlag(SusceptibleTo))
+            OnReceiveDamage(hurt);
+    }
+
+    protected abstract void OnReceiveDamage(HurtBox hurtbox);
+
+
+}
