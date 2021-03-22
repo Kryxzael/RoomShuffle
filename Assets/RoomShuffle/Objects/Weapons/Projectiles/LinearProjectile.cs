69cb16b41158c9a2b890b61464e5b6b233ddab63
Tormod Kvitberg <tormod@kvitberg.no>
2021-03-22 21:11:52 +0100

    Add splitting projectile. Refactoring code

--- Assets/RoomShuffle/Objects/Weapons/Projectiles/LinearProjectile.cs
+++ Assets/RoomShuffle/Objects/Weapons/Projectiles/LinearProjectile.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class LinearProjectile : Projectile
@@ -18,6 +12,7 @@ public class LinearProjectile : Projectile
 
     private void Update()
     {
+        //sets the speed in the facing direction
         _rigidbody.velocity = transform.up * Speed;
     }
 }
--- Assets/RoomShuffle/Objects/Weapons/Projectiles/SinewaveProjectile.cs
+++ Assets/RoomShuffle/Objects/Weapons/Projectiles/SinewaveProjectile.cs
@@ -1,36 +1,32 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class SinewaveProjectile : Projectile
 {
+    [Tooltip("The speed of the amplitude of the wave")]
+    public float WaveSpeed = 10f;
+    
     private Vector3 linearPosition;
     private float time;
     
     protected void Awake()
     {
         linearPosition = transform.position;
-        
     }
 
     private void Update()
     {
-        const float SPEED = 10f;
         time += Time.deltaTime;
-
+        
         //Update position in firing direction
         transform.position = linearPosition;
-        transform.position += transform.up * SPEED * Time.deltaTime;
+        transform.position += transform.up * (Speed * Time.deltaTime);
         linearPosition = transform.position;
 
         //Update position in wavy direction
         int invert = (transform.up.x > 0) ? -1 : 1;
-        transform.position += transform.right * (float)Math.Sin(time * 10f) * 0.75f * invert;
+        transform.position += transform.right * ((float)Math.Sin(time * WaveSpeed) * 0.75f * invert);
 
 
     }
