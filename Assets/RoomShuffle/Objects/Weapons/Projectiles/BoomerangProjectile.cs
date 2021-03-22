2c2312c1b21af9ede12a765b157d9550bdf7164e
Tormod Kvitberg <tormod@kvitberg.no>
2021-03-22 17:49:47 +0100

    Add boomerang power-up

---
+++ Assets/RoomShuffle/Objects/Weapons/Projectiles/BoomerangProjectile.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BoomerangProjectile : Projectile
+{
+    private Rigidbody2D _rigidbody;
+
+    protected void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidbody.velocity = transform.up * Speed;
+    }
+
+    private void Update()
+    {
+        const float PUSHBACKFORCE = 1000f; // recommended 1000
+        _rigidbody.AddForce(-transform.up * (Time.deltaTime * PUSHBACKFORCE));
+    }
+}
