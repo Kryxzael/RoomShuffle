33994fd694ee1c21c98bea58f20cbf5ae83e4744
Tormod Kvitberg <tormod@kvitberg.no>
2021-03-23 21:03:14 +0100

    Add Spraying gun 👍

--- Assets/RoomShuffle/Objects/Weapons/Projectiles/FallingProjectile.cs
+++ Assets/RoomShuffle/Objects/Weapons/Projectiles/FallingProjectile.cs
@@ -1,18 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+public class FallingProjectile : Projectile
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D _rigidbody;
+    private GameObject _shooter;
+
+    protected void Start()
     {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _shooter = GetComponent<WeaponFireHurtbox>().Shooter.gameObject;
         
+        //Makes the bullet affected by gravity
+        _rigidbody.gravityScale = 1;
+        
+        //sets the speed in the facing direction
+        _rigidbody.velocity = transform.up * Speed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        //Return if the bullets collides with the shooter or another bullet
+        if (other.gameObject.tag.Equals(_shooter.tag) || other.gameObject.tag.Equals("Projectile"))
+            return;
         
+        _rigidbody.velocity = Vector2.zero;
     }
 }
--- Assets/RoomShuffle/Objects/Weapons/WeaponTemplates/SprayingWeapon.cs
+++ Assets/RoomShuffle/Objects/Weapons/WeaponTemplates/SprayingWeapon.cs
@@ -1,32 +1,38 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 /// <summary>
 /// A weapon that shoots multiple bullets forwards in a cone
 /// </summary>
-[CreateAssetMenu(menuName = "Weapons/Cone Shot")]
-public class ConeShotWeapon : WeaponTemplate
+[CreateAssetMenu(menuName = "Weapons/Spraying Weapon")]
+public class SprayingWeapon : WeaponTemplate
 {
     [Tooltip("The ammunition to use when firing")]
     public Projectile Ammunition;
 
     [Tooltip("The largest angle offset the bullet can have from the direction the shooter is actually facing")]
-    public float MaxAngle;
+    [Range(0, 360)] public int MaxAngle;
 
     [Tooltip("How many bullets will be fired in one blast")]
-    public int BlastCount;
+    [Range(1, 10)] public int BlastCount = 2;
+    
 
     protected override void OnFire(WeaponInstance instance, WeaponShooterBase shooter, Vector2 direction)
     {
+
         for (int i = 0; i < BlastCount; i++)
         {
+
             Projectile newAmmo = Instantiate(
                 original: Ammunition,
                 position: shooter.GetProjectilesSpawnPoint(),
                 rotation: Quaternion.identity
             );
 
+            //Rotates the bullet random amount
+            RandomValueBetween randomAngle = new RandomValueBetween(-MaxAngle/2, MaxAngle/2);
+            int angle = randomAngle.PickInt();
             newAmmo.Direction = direction;
-            newAmmo.transform.Rotate((i - BlastCount / 2) * MaxAngle);
+            newAmmo.transform.Rotate(angle);
 
             WeaponFireHurtbox hurtbox = newAmmo.GetComponent<WeaponFireHurtbox>();
             hurtbox.Shooter = shooter;
