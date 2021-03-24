e400e370c111d05891cbe6af67e8905541e0b61d
Kryxzael <kasper.ramskjell98@gmail.com>
2021-03-24 21:25:09 +0100

    Add spikes

---
+++ Assets/RoomShuffle/Objects/Health/HurtBoxes/GlobalHurtbox.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+/// <summary>
+/// A simple hurtbox that damages all types of objects
+/// </summary>
+public class GlobalHurtbox : HurtBox
+{
+    [Tooltip("The amount of damage the hurtbox will do")]
+    public int Damage;
+
+    /// <summary>
+    /// <inheritdoc />
+    /// </summary>
+    /// <returns></returns>
+    public override int GetDamage()
+    {
+        return Damage;
+    }
+
+    /// <summary>
+    /// <inheritdoc />
+    /// </summary>
+    /// <returns></returns>
+    public override HurtBoxTypes GetTargets()
+    {
+        return HurtBoxTypes.HurtfulToEnemies | HurtBoxTypes.HurtfulToPlayer;
+    }
+}
