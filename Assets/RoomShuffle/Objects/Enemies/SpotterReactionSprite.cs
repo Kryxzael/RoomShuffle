a21d02faabd582e049ea0988a112b723b26c0dae
Tormod Kvitberg <tormod@kvitberg.no>
2021-03-24 21:36:14 +0100

    Add reaction sprite to spotter enemies

---
+++ Assets/RoomShuffle/Objects/Enemies/SpotterReactionSprite.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpotPlayer))]
+public class SpotterReactionSprite : MonoBehaviour
+{
+    public Sprite PuzzeledSprite;
+
+    public Sprite SpottedSprite;
+    
+    public Sprite ChasingSprite;
+    
+    public Sprite BlindChasingSprite;
+
+
+    public SpriteRenderer ReactionSpriteRenderer;
+
+    private SpotterPlayerRelationship _lastState;
+    private SpotPlayer _spotPlayer;
+    void Start()
+    {
+        _spotPlayer = GetComponent<SpotPlayer>();
+        _lastState = _spotPlayer.State;
+    }
+    
+    void Update()
+    {
+        if (_lastState == _spotPlayer.State)
+            return;
+
+        _lastState = _spotPlayer.State;
+
+        ReactionSpriteRenderer.sprite = GetReactionSprite(_spotPlayer.State);
+    }
+    
+    /// <summary>
+    /// Return the sprite that suits the spotter's state
+    /// </summary>
+    /// <param name="state"></param>
+    /// <returns></returns>
+    private Sprite GetReactionSprite(SpotterPlayerRelationship state)
+    {
+        switch (state)
+        {
+            case SpotterPlayerRelationship.Spotted:
+                return SpottedSprite;
+            case SpotterPlayerRelationship.Puzzled:
+                return PuzzeledSprite;
+            case SpotterPlayerRelationship.Chasing:
+                return ChasingSprite;
+            case SpotterPlayerRelationship.BlindChasing:
+                return BlindChasingSprite;
+            default:
+                return null;
+        }
+    }
+}
