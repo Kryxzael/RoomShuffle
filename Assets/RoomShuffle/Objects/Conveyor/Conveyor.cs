5477cc2b924e1a2cc24d35c9a7ccdc67c8f4a51f
Kryxzael <kasper.ramskjell98@gmail.com>
2021-03-25 02:10:01 +0100

    Add conveyors

---
+++ Assets/RoomShuffle/Objects/Conveyor/Conveyor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+/// <summary>
+/// An object that pushes objects on it in a direction
+/// </summary>
+public class Conveyor : DetectObjectsOn
+{
+    [Tooltip("The direction the conveyor will move its objects")]
+    public Direction1D Directionality;
+
+    [Tooltip("The speed the conveyor will move at")]
+    public float Speed;
+
+    private void LateUpdate()
+    {
+        foreach (Transform i in ObjectsOn)
+        {
+            i.Translate(new Vector3((int)Directionality * Commons.GetEffectValue(Speed, EffectValueType.EnemySpeed), 0f));
+        }
+    }
+}
