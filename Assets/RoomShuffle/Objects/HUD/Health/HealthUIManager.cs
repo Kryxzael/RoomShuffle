600e0c5070bbb4bf75fc241c01677355f560e3aa
Kryxzael <kasper.ramskjell98@gmail.com>
2021-03-27 13:50:11 +0100

    Add health change animation

--- Assets/RoomShuffle/Objects/HUD/Health/HealthUIManager.cs
+++ Assets/RoomShuffle/Objects/HUD/Health/HealthUIManager.cs
@@ -28,12 +28,15 @@ public class HealthUIManager : MonoBehaviour
     //The owner's last known health
     private int _lastHealthLevel;
 
+    //Is the health changing animation currently running?
+    private bool _healthChangeAnimationRunning;
+
     void Start()
     {
         _lastHealthLevel = Commons.PlayerProgression.HealthLevel;
         SetHeartUIElementCount(_lastHealthLevel+3);
 
-        RectTransform rectTransform = transform.GetComponent<RectTransform>();
+        RectTransform rectTransform = (RectTransform)transform;
         rectTransform.sizeDelta = new Vector2 (HeartsPerRow * HeartMargin.x * 2, 500); //Where does this number come from?
     }
 
@@ -46,17 +49,52 @@ public class HealthUIManager : MonoBehaviour
         {
             //Update healthlevel
             _lastHealthLevel = Commons.PlayerProgression.HealthLevel;
-            SetHeartUIElementCount(_lastHealthLevel+3);
+            SetHeartUIElementCount(Commons.PlayerProgression.GetMaximumHealth() / HealthController.HP_PER_HEART);
 
         }
         
         //The player's health has changed
         if (_lastHealth != playerHealth)
         {
+            int animationInitialHealth = _lastHealth;
             _lastHealth = playerHealth;
-            SetDisplayedHealth(_lastHealth);
+            AnimateHeartsToCurrentHealth(animationInitialHealth);
         }
 
+    }
+
+    /// <summary>
+    /// Starts or continues an animation of loosing or gaining health from the provided starting health to the current health value
+    /// </summary>
+    /// <param name="initialHealth"></param>
+    public void AnimateHeartsToCurrentHealth(int initialHealth)
+    {
+        //Increase size of affected hearts
+        {
+            int firstAffectedHeart = Math.Min(initialHealth, _lastHealth) / HealthController.HP_PER_HEART;
+            int lastAffectedHeart = Math.Max(initialHealth, _lastHealth) / HealthController.HP_PER_HEART;
+
+            int index = 0;
+
+
+            foreach (Heart i in _heartStack.Reverse())
+            {
+                if (index >= lastAffectedHeart)
+                    break;
+
+                if (index >= firstAffectedHeart)
+                    i.transform.localScale = Vector3.one * 1.5f;
+
+                index++;
+            }
+        }
+            
+
+        if (!_healthChangeAnimationRunning)
+        {
+            _healthChangeAnimationRunning = true;
+            StartCoroutine(CoAnimateHealthChange(initialHealth));
+        }
     }
 
     /// <summary>
@@ -69,7 +107,7 @@ public class HealthUIManager : MonoBehaviour
         foreach (Heart heart in _heartStack.Reverse())
         {
             //If heart should get maxed out
-            if (health - HealthController.HP_PER_HEART > 0 )
+            if (health - HealthController.HP_PER_HEART > 0)
             {
                 //Fill heart 100%
                 heart.SetHeartFillPercentage(1f);
@@ -78,10 +116,59 @@ public class HealthUIManager : MonoBehaviour
             else
             {
                 //fill heart partly
-                heart.SetHeartFillPercentage(health / HealthController.HP_PER_HEART);
+                heart.SetHeartFillPercentage((float)health / HealthController.HP_PER_HEART);
                 health = 0;
             }
         }
+    }
+
+    /// <summary>
+    /// Animates a health change from the provided health to the player's current health
+    /// </summary>
+    /// <param name="initialHealth"></param>
+    /// <returns></returns>
+    private IEnumerator CoAnimateHealthChange(int initialHealth)
+    {
+        //Keeps track of the health that is _actualy_ being displayed at this time
+        int currentlyDisplayedHealth = initialHealth;
+
+        //Wait a bit, for dramatic effect
+        yield return new WaitForSecondsRealtime(0.75f);
+
+        //As long as the displayed health doesn't match the player's actual health, we keep the animation going
+        while (currentlyDisplayedHealth != _lastHealth)
+        {
+            //Calculate the difference between the displayed health and the player's actual health
+            int difference = Math.Abs(currentlyDisplayedHealth - _lastHealth);
+
+            //How much HP to animate per unit of time
+            int changeAmount = 10;
+
+            //If we are more than one heart from our target, we can move a little faster
+            if (difference > HealthController.HP_PER_HEART)
+                changeAmount = 20;
+
+            //If we are less (or equal for effect) than `changeAmount` HP away from our target, reduce the speed to 1 so we don't overshoot
+            if (difference <= changeAmount)
+                changeAmount = 1;
+
+            //The health is moving down, inverse the change
+            if (currentlyDisplayedHealth > _lastHealth)
+                changeAmount *= -1;
+
+            //Apply the change and update the display
+            currentlyDisplayedHealth += changeAmount;
+            SetDisplayedHealth(currentlyDisplayedHealth);
+
+            yield return new WaitForSecondsRealtime(0.05f);
+        }
+
+        //Reset size of hearts
+        foreach (Heart i in _heartStack)
+            i.transform.localScale = Vector3.one;
+
+        //Stop animation
+        _healthChangeAnimationRunning = false;
     }
 
     /// <summary>
@@ -103,6 +190,7 @@ public class HealthUIManager : MonoBehaviour
             );
 
             _heartStack.Push(newHeart);
+            newHeart.SetHeartFillPercentage(0f);
         }
     }
 
