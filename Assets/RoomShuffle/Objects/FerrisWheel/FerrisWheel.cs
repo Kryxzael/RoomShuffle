30112f23b4d54ba1908046c87af08f8d365e5723
Tormod Kvitberg <tormod@kvitberg.no>
2021-04-04 03:31:48 +0200

    Add "Breathing" ferris wheels

--- Assets/RoomShuffle/Objects/FerrisWheel/FerrisWheel.cs
+++ Assets/RoomShuffle/Objects/FerrisWheel/FerrisWheel.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class FerrisWheel : MonoBehaviour
@@ -19,8 +21,21 @@ public class FerrisWheel : MonoBehaviour
     [Tooltip("The radius of the ferris wheel")] 
     public float Radius;
 
+    [Tooltip("How fast the circle expands and shrinks")]
+    public float BreathSpeed;
+    
+    [Tooltip("How much the circle shrinks and expands")]
+    public float BreathAmplitude;
+
+    //List of all circularMotions on the ferris wheel
+    private List<CircularMotion> cmList = new List<CircularMotion>();
+    
+    //The time used to keep track of the "breathing"
+    private float _time;
+
     void Start()
     {
+
         for (int i = 0; i < NumberOfPlatforms; i++)
         {
             //instantiate the object and get circular motion
@@ -30,6 +45,9 @@ public class FerrisWheel : MonoBehaviour
             if (circularMotion == null)
                 circularMotion = obj.AddComponent<CircularMotion>();
 
+            //Add object to list
+            cmList.Add(circularMotion);
+
             //sets the angle
             circularMotion.SetAngle(360 / (NumberOfPlatforms + NumberOfBlankSpace) * i);
             
@@ -37,6 +55,24 @@ public class FerrisWheel : MonoBehaviour
             circularMotion.Radius = Radius;
             circularMotion.Speed = Speed;
             
+        }
+    }
+
+    private void Update()
+    {
+        _time += Time.deltaTime;
+        
+        //foreach circularmotion on the ferris wheel
+        foreach (CircularMotion cm in cmList)
+        {
+            //Edit the radius of the circularmotion to expand or shrink (from 1 * BreathAmplitude to -1 * Breathamplitude)
+            cm.Radius = Radius + (float)Math.Sin(_time * BreathSpeed) * BreathAmplitude;
+        }
+        
+        //Keeps the _time variable low
+        if (_time > (float)Math.PI * 2)
+        {
+            _time -= (float)Math.PI * 2;
         }
     }
 }
