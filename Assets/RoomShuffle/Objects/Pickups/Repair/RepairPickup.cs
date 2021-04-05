36d5fb26e045a6898dcea9de8af7aa2136b6ce00
Kryxzael <kasper.ramskjell98@gmail.com>
2021-04-05 23:31:59 +0200

    Add weapon-repair item

---
+++ Assets/RoomShuffle/Objects/Pickups/Repair/RepairPickup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Repairs the selected weapon when picked up
+/// </summary>
+public class RepairPickup : PickupScript
+{
+    public override void OnPickup()
+    {
+        if (Commons.Inventory.SelectedWeapon != null)
+            Commons.Inventory.SelectedWeapon.Durability = Commons.Inventory.SelectedWeapon.MaxDurability;
+    }
+}
