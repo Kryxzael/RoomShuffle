f77c0c5826fc1da324118a0abef977ae0375ad62
Kryxzael <kasper.ramskjell98@gmail.com>
2021-04-09 17:31:13 +0200

    Add eradication locks. Solid blocks that are destroyed when all enemies are killed

---
+++ Assets/RoomShuffle/Objects/LockedBlock/EradicationLockBlock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+/// <summary>
+/// A solid block that is destroyed when all enemies in the room are defeated
+/// </summary>
+public class EradicationLockBlock : MonoBehaviour
+{
+    /// <summary>
+    /// Gets how many enemies are left in the stage
+    /// </summary>
+    public static int EnemiesLeft
+    {
+        get
+        {
+            return FindObjectsOfType<EnemyBase>().Count();
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (EnemiesLeft == 0)
+            Destroy(gameObject);
+    }
+}
--- Assets/RoomShuffle/Objects/Markers/EnemyBlocker/EnemyBlockerDisplay.cs
+++ Assets/RoomShuffle/Objects/Markers/EnemyBlocker/EnemyBlockerDisplay.cs
@@ -3,24 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
-[ExecuteInEditMode]
 public class EnemyBlockerDisplay : MonoBehaviour
 {
-    private Collider2D _collider;
-
-    private void Awake()
-    {
-        _collider = GetComponent<Collider2D>();
-    }
-
     private void OnDrawGizmos()
     {
+        var collider = GetComponent<Collider2D>();
+
         Gizmos.color = Color.green;
 
-        if (_collider is BoxCollider2D)
-            Gizmos.DrawWireCube(_collider.bounds.center, _collider.bounds.size);
+        if (collider is BoxCollider2D)
+            Gizmos.DrawWireCube(collider.bounds.center, collider.bounds.size);
 
-        else if (_collider is CircleCollider2D)
-            Gizmos.DrawWireSphere(_collider.bounds.center, _collider.bounds.size.magnitude / 2f);
+        else if (collider is CircleCollider2D)
+            Gizmos.DrawWireSphere(collider.bounds.center, collider.bounds.size.magnitude / 2f);
     }
 }
