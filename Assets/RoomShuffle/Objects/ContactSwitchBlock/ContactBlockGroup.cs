3fa86fd8f6af008edbfeb749141c53c89f7196cf
Tormod Kvitberg <tormod@kvitberg.no>
2021-04-14 21:50:50 +0200

    Add contact Switch blocks

---
+++ Assets/RoomShuffle/Objects/ContactSwitchBlock/ContactBlockGroup.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework.Constraints;
+using UnityEngine;
+
+public class ContactBlockGroup : MonoBehaviour
+{
+    private List<ContactSwitch> _blocks = new List<ContactSwitch>();
+    private GameObject _prize;
+
+    private bool prizeSpawned = false;
+    void Start()
+    {
+        //Add all contactswitch blocks to list
+        foreach (Transform child in transform)
+        {
+            if (child.name == "Blocks")
+            {
+                foreach (Transform childchild in child)
+                {
+                    _blocks.Add(childchild.GetComponent<ContactSwitch>());
+                }
+            }
+            
+            if (child.name == "Prize")
+            {
+                _prize = child.gameObject;
+            }
+        }
+        
+        _prize.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_blocks.TrueForAll(x => x.On))
+        {
+            _prize.SetActive(true);
+        }
+    }
+}
---
+++ Assets/RoomShuffle/Objects/ContactSwitchBlock/ContactSwitch.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContactSwitch : MonoBehaviour
+{
+
+    public bool On = false;
+    public Sprite OnSprite;
+    public Sprite OffSprite;
+
+    private SpriteRenderer _sprite;
+
+    private void Awake()
+    {
+        _sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.IsPlayer())
+        {
+            On = !On;
+
+            _sprite.sprite = On ? OnSprite : OffSprite;
+        }
+    }
+}
