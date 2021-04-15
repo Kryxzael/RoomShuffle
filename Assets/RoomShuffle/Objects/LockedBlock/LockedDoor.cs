c2cf562007f1bdb2146edebb2d7d3b4d96049f05
Kryxzael <kasper.ramskjell98@gmail.com>
2021-04-15 20:25:04 +0200

    Add secret doors, and secret-room parameter overrider

---
+++ Assets/RoomShuffle/Objects/LockedBlock/LockedDoor.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A door that the player can enter if they have a general key
+/// </summary>
+public class LockedDoor : MonoBehaviour
+{
+    private bool _playerInRange;
+
+    [Tooltip("The room parameter override object that will generate the secret room")]
+    public ParameterBuilderOverride GeneratorOverride;
+
+    private void Update()
+    {
+        if (_playerInRange && Input.GetButtonDown("Interact"))
+        {
+            if (Commons.Inventory.GeneralKeys > 0)
+            {
+                Commons.Inventory.GeneralKeys--;
+
+                Commons.RoomGenerator.RoomParameterBuilderOverrides.Push(GeneratorOverride);
+                Commons.RoomGenerator.GenerateNext();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.IsPlayer())
+            _playerInRange = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.IsPlayer())
+            _playerInRange = false;
+    }
+}
---
+++ Assets/RoomShuffle/RoomGeneration/ParameterBuilders/SecretRoomOverride.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+/// <summary>
+/// A parameter picker that generates secret rooms
+/// </summary>
+[CreateAssetMenu(menuName = "Room Parameter Builders/Secret")]
+public class SecretRoomOverride : ParameterBuilderOverride
+{
+    public bool HasGenerated { get; private set; }
+
+    public override RoomParameters GetInitialParameters(System.Random random)
+    {
+        HasGenerated = true;
+
+        return new RoomParameters()
+        {
+            Class = RoomClass.Secret,
+            Effect = RoomEffects.None,
+            EnemySet = null,
+            Entrance = EntranceExitSides.Left,
+            Exit = EntranceExitSides.Right,
+            FlipHorizontal = random.Next(2) == 0,
+            Layout = Rooms.SecretRooms[random.Next(Rooms.SecretRooms.Count)],
+            Theme = RoomTheme.Autumn,
+            WeaponEnumerator = null
+        };
+    }
+
+    public override RoomParameters GetNextParameters(RoomHistory history, System.Random random)
+    {
+        return GetInitialParameters(random);
+    }
+
+    public override bool HasNext()
+    {
+        return !HasGenerated;
+    }
+}
