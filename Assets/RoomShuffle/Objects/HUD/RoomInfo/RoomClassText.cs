701728e33c86984f5bd41db2d8250ecc67e7000a
Kryxzael <kasper.ramskjell98@gmail.com>
2021-04-20 22:41:19 +0200

    Add room-class and -effect labels to HUD

---
+++ Assets/RoomShuffle/Objects/HUD/RoomInfo/RoomClassText.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TMPro;
+
+using UnityEngine;
+
+/// <summary>
+/// Displays the name of the current room's class
+/// </summary>
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class RoomClassText : MonoBehaviour
+{
+    private TextMeshProUGUI _label;
+
+    private void Awake()
+    {
+        _label = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        _label.text = Commons.RoomGenerator.CurrentRoomConfig.Class switch
+        {
+            RoomClass.Inaccessible => "Unknown",
+            RoomClass.Starting => "",
+            RoomClass.Platforming => "",
+            RoomClass.Respite => "",
+            RoomClass.Transition => "",
+            RoomClass.Shop => "Shop",
+            RoomClass.Eradication => "Eradication",
+            RoomClass.Puzzle => "Puzzle",
+            RoomClass.Secret => "Secret",
+            RoomClass.Crossroads => "",
+            RoomClass.Boss => "Boss",
+            _ => throw new NotImplementedException(),
+        };
+    }
+}
---
+++ Assets/RoomShuffle/Objects/HUD/RoomInfo/RoomEffectText.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TMPro;
+
+using UnityEngine;
+
+/// <summary>
+/// Displays the name of the current room's effect
+/// </summary>
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class RoomEffectText : MonoBehaviour
+{
+    private TextMeshProUGUI _label;
+
+    private void Awake()
+    {
+        _label = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        _label.text = Commons.RoomGenerator.CurrentRoomConfig.Effect switch
+        {
+            RoomEffects.None => "",
+            RoomEffects.LowGravity => "Low Gravity",
+            RoomEffects.Darkness => "Dark",
+            RoomEffects.ValuePickups => "2x Pickups",
+            RoomEffects.FastFoe => "Fast-Foe",
+            RoomEffects.LargeEnemies => "Large Enemies",
+            RoomEffects.LargeProjectiles => "Large Projectiles",
+            RoomEffects.Backlit => "Back-lit",
+            RoomEffects.ReverseControls => "Reversed Controls",
+            RoomEffects.Icy => "Slippery",
+            RoomEffects.Timer => "Time Limit",
+            _ => throw new NotImplementedException(),
+        };
+    }
+}
