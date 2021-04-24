c3c49a21f88d3275ee8c6d90e1f70bb15fa95668
Kryxzael <kasper.ramskjell98@gmail.com>
2021-04-24 16:46:12 +0200

    Add damage-level-up pickup

---
+++ Assets/RoomShuffle/Objects/Pickups/LevelUp/DamageLevelPickup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Levels up the player's damage power
+/// </summary>
+public class DamageLevelPickup : PickupScript
+{
+    public override void OnPickup()
+    {
+        //Add one to health level
+        Commons.PlayerProgression.LevelUpDamage();
+    }
+}
