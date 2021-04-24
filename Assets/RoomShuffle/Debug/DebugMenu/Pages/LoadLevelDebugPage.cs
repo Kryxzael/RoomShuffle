9f8500f62796e09da6702f3b0191afa41fe0ce88
Kryxzael <kasper.ramskjell98@gmail.com>
2021-04-24 16:56:40 +0200

    Add room loading debug page that _isn't_ broken

--- Assets/RoomShuffle/Debug/DebugMenu/Pages/LoadLevelDebugPage.cs
+++ Assets/RoomShuffle/Debug/DebugMenu/Pages/LoadLevelDebugPage.cs
@@ -12,7 +12,44 @@ public class LoadLevelDebugPage : DebugPage
 
     protected override void RunItems(DebugMenu caller)
     {
-        foreach (var i in Resources.FindObjectsOfTypeAll<RoomLayout>().OrderBy(i => i.name))
+        if (Button("Platformers ... "))
+            caller.NavigationStack.Push(new LoadLevelFromListDebugPage("Platformers", Commons.RoomGenerator.RoomParameterBuilder.Rooms.PlatformingRooms));
+
+        if (Button("Puzzles ... "))
+            caller.NavigationStack.Push(new LoadLevelFromListDebugPage("Puzzles", Commons.RoomGenerator.RoomParameterBuilder.Rooms.PuzzleRooms));
+
+        if (Button("Eradications ... "))
+            caller.NavigationStack.Push(new LoadLevelFromListDebugPage("Eradications", Commons.RoomGenerator.RoomParameterBuilder.Rooms.PlatformingRooms));
+
+        if (Button("Shops ... "))
+            caller.NavigationStack.Push(new LoadLevelFromListDebugPage("Shops", Commons.RoomGenerator.RoomParameterBuilder.Rooms.ShopRooms));
+
+        if (Button("Transitions ... "))
+            caller.NavigationStack.Push(new LoadLevelFromListDebugPage("Transitions", Commons.RoomGenerator.RoomParameterBuilder.Rooms.TransitionRooms));
+
+        if (Button("Respite ... "))
+            caller.NavigationStack.Push(new LoadLevelFromListDebugPage("Respite", Commons.RoomGenerator.RoomParameterBuilder.Rooms.RespiteRooms));
+
+        if (Button("Openings ... "))
+            caller.NavigationStack.Push(new LoadLevelFromListDebugPage("Openings", Commons.RoomGenerator.RoomParameterBuilder.Rooms.StartingRooms));
+    }
+}
+
+public class LoadLevelFromListDebugPage : DebugPage
+{
+    public override string Header { get; }
+    private IEnumerable<RoomLayout> Layouts { get; }
+
+    public LoadLevelFromListDebugPage(string header, IEnumerable<RoomLayout> layouts)
+    {
+        Header = header;
+        Layouts = layouts.OrderBy(i => i.name);
+    }
+
+
+    protected override void RunItems(DebugMenu caller)
+    {
+        foreach (var i in Layouts)
         {
             if (Button(i.name))
             {
@@ -22,7 +59,7 @@ public class LoadLevelDebugPage : DebugPage
                 Commons.RoomGenerator.RoomParameterBuilderOverrides.Push(roomOverride);
                 Commons.RoomGenerator.GenerateNext();
             }
-                
+
         }
     }
 }
