b74e71e315e609223eba8b4e42045398b6909b7f
Kryxzael <kasper.ramskjell98@gmail.com>
2021-05-01 19:35:41 +0200

    More polish of stages

---
+++ Assets/RoomShuffle/Objects/Pickups/Weapon/RouletteWeaponPickup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+public class RouletteWeaponPickup : WeaponPickupBase
+{
+    private WeaponInstance _weapon;
+    private SpriteRenderer _renderer;
+
+    protected override void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+        ChangeWeapon();
+        InvokeRepeating(nameof(ChangeWeapon), 0.25f, 0.25f);
+        base.Start();
+    }
+
+    public void ChangeWeapon()
+    {
+        _weapon = Commons.RoomGenerator.RoomParameterBuilder.WeaponTemplates[UnityEngine.Random.Range(0, Commons.RoomGenerator.RoomParameterBuilder.WeaponTemplates.Count)]
+            .CreateWeaponInstance();
+
+        _renderer.sprite = GetWeapon().Template.Icon;
+    }
+
+    public override WeaponInstance GetWeapon()
+    {
+        return _weapon;
+    }
+}
