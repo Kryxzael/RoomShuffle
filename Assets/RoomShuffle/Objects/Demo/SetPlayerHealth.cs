825692bedc9a5ca0719b928c22c013b1f7055959
Kryxzael <kasper.ramskjell98@gmail.com>
2021-06-03 17:54:36 +0200

    Add respite demo-room

---
+++ Assets/RoomShuffle/Objects/Demo/SetPlayerHealth.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+/// <summary>
+/// Sets the player's health when the object spawns
+/// </summary>
+public class SetPlayerHealth : MonoBehaviour
+{
+    [Tooltip("The health the player will get when the object spawns")]
+    public int PlayerHealth = 50;
+
+    private void Start()
+    {
+        Commons.PlayerHealth.Health = PlayerHealth;
+    }
+}
